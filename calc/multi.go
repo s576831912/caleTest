afc60e25f63e2270d81e66c9401f61cc362afa63
unknown <ssz>
2019-11-07 14:52:37 +0800

    实现乘法函数

---
+++ calc/multi.go
@@ -0,0 +1,5 @@
+package calc
+
+func Mulit(a,b int)int  {
+	return a*b
+}
--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	fmt.Println("calc add called!")
-	res := calc.Add(10,20)
+	res := calc.Add(10, 20)
 	fmt.Println("Add(10,20): ", res)
 	fmt.Println("calc sub called!")
-	res = calc.Sub(30,20)
+	res = calc.Sub(30, 20)
 	fmt.Println("Sub(10,20): ", res)
+
+	fmt.Println("calc multi called!")
+	res = calc.Mulit(10,20)
+	fmt.Println("multi(10,20): ", res)
 }
