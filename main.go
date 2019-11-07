8daae51a665b33ac6102c77f593ab422b5671fcd
unknown <ssz>
2019-11-07 10:48:10 +0800

    添加sub函数

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println("add called!")
+	fmt.Println("calc add called!")
 	res := calc.Add(10,20)
 	fmt.Println("Add(10,20): ", res)
+	fmt.Println("calc sub called!")
+	res = calc.Sub(30,20)
+	fmt.Println("Sub(10,20): ", res)
 }
